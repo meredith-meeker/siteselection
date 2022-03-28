7bfec089227575deeafa40e645c163a7b2b1c486
meredith-meeker <mmeeker118@gmail.com>
2022-03-28 19:24:18 -0400

    initial scatter plots

---
+++ Site Selection.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(ggplot2)
+library(lmtest)
+library(zoo)
+library(car)
+library(pwr)
+site_selection = read.csv('D:/GIS Files/City of Ottawa Ponds and Parks/Site Selection Criteria.csv')
+summary(site_selection)
+range(site_selection$Area..ha.)
+Area <- site_selection$Area..ha.
+Habitat <- site_selection$Habitat.Unit.Id
+Natural <- site_selection$X..Natural.Landcover
+Vegetation <- site_selection$Vegetation.Heterogeneity.Score.
+Impervious <- site_selection$X..Impervious.Area
+
+natural_x_veg_graph1 <-ggplot(data = site_selection) +
+  geom_point(mapping = aes(x = Natural, y = Vegetation))
+natural_x_veg_graph1
+impervious_x_veg_graph2 <-ggplot(data = site_selection) +
+  geom_point(mapping = aes(x = Impervious, y = Vegetation))
+impervious_x_veg_graph2
